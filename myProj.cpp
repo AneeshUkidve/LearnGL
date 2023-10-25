71e9a169a006e47e067682aedc2b81a81820b985
Aneesh Ukidve <aneeshukidve@gmail.com>
2023-10-26 03:31:42 +0530

    please i beg of you

--- myProj.cpp
+++ myProj.cpp
@@ -2,20 +2,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h> 
 #include <shader.h>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 790804c (my lord this was a fun one)
 #include <stb_image.h>
 #include <cmath>
-=======
->>>>>>> c0e4f66 (We have class shader and different files for)
-
-float vis = 0.0f;
-=======
-#include <cmath>
->>>>>>> 5ca42b6 (fun with shaders complete)
 
 float vis = 0.0f;
 
@@ -65,93 +53,17 @@ int main()
 	glViewport(0, 0, 800, 600);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); 
 
-
-	//Shader code
-	//Vertex Shader
-	const char *vertexShaderSource = "#version 330 core\n"
 	Shader ourShader("shaders/shader.vs", "shaders/shader.fs");
-	"layout (location = 1) in vec3 aColor;\n"
-	"out vec3 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-	"	ourColor = aColor;\n"
-    "}\0";
 
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-
-	int  success;
-	char infoLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
-	if(!success){
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "Vertex Shader Compilation Failed\n" << infoLog << std::endl;
-	}
-	stbi_image_free(data);
-
-	unsigned int texture2;
-	glGenTextures(1, &texture2);
-	glBindTexture(GL_TEXTURE_2D, texture2);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-	
-	//borderColor[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	stbi_set_flip_vertically_on_load(true);
-	unsigned char *data2 = stbi_load("textures/awesomeface.png", &width, &height, &nrChannels, 0);
-
-	if (data2){
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data2);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else{
-		std::cout << "FAILED TO LOAD TEXTURE" << std::endl;
-	}
-	stbi_image_free(data2);
-
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-////////////////////////////////////////////////////////////////////////////////////////////////////
-	//version2
 	
 	//Triangle code below
 	float vertices[] = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-		0.5f,  0.5f, 0.0f,  // top right
-		0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f   // top left 
-	};
-	unsigned int indices[] = {  // note that we start from 0!
-		0, 2, 3,   // first triangle
-		1, 2, 0    // second triangle
-	};  
-
-=======
-		// positions         // colors
-		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
-	};    
-=======
 		// positions          // colors           // texture coords
 		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
 		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
 		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
 		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
 	};
->>>>>>> 44b3788 (everything works)
 	
 	unsigned int indices[] = {
 		0, 1, 2,
@@ -192,9 +104,6 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 	
-<<<<<<< HEAD
->>>>>>> 790804c (my lord this was a fun one)
-=======
 	//borderColor[] = { 1.0f, 1.0f, 1.0f, 1.0f };
 	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
 
@@ -214,7 +123,6 @@ int main()
 	stbi_image_free(data2);
 
 
->>>>>>> 44b3788 (everything works)
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
@@ -247,63 +155,24 @@ int main()
 
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	std::cout << "Main" << std::endl;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 44b3788 (everything works)
 	ourShader.use();
 	ourShader.setInt("otexture1", 0);
 	ourShader.setInt("otexture2", 1);
 	ourShader.setFloat("visiblity", vis);
-<<<<<<< HEAD
-=======
->>>>>>> 897f1c4 (savepoint)
-=======
->>>>>>> 44b3788 (everything works)
 
 	while(!glfwWindowShouldClose(window))
 	{
 		processInput(window);
-<<<<<<< HEAD
-<<<<<<< HEAD
 		glClearColor(0.0f, 0.2f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 	
 		ourShader.use();
 		ourShader.setFloat("visiblity", vis);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 44b3788 (everything works)
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture1);
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture2);
-<<<<<<< HEAD
-=======
-=======
-		glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-	
-	    float timeValue = glfwGetTime();
-		float offset1 = sin(timeValue) / 2.0f;
-<<<<<<< HEAD
-		float offset2 = cos(timeValue) / 2.0f;
->>>>>>> 5ca42b6 (fun with shaders complete)
-=======
-		float offset2 = cos(timeValue/2.0f) / 2.0f;
->>>>>>> 897f1c4 (savepoint)
-=======
-		glClearColor(0.0f, 0.2f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-	
->>>>>>> 790804c (my lord this was a fun one)
-		ourShader.use();
-
->>>>>>> c0e4f66 (We have class shader and different files for)
-=======
->>>>>>> 44b3788 (everything works)
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
